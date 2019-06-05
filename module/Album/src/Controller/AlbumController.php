b1fa1ddc1b1f5414626ab50569f93c5d3721949e
Antoine87 <antoine.madry@outlook.fr>
2019-06-05 11:07:05 +0200

    Forms and actions: Adding new albums

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Album\Controller;
 
+use Album\Form\AlbumForm;
+use Album\Model\Album;
 use Album\Model\AlbumTable;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
@@ -26,6 +28,26 @@ class AlbumController extends AbstractActionController
 
     public function addAction()
     {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+
+        if (! $request->isPost()) {
+            return ['form' => $form];
+        }
+
+        $album = new Album();
+        $form->setInputFilter($album->getInputFilter());
+        $form->setData($request->getPost());
+
+        if (! $form->isValid()) {
+            return ['form' => $form];
+        }
+
+        $album->exchangeArray($form->getData());
+        $this->table->saveAlbum($album);
+        return $this->redirect()->toRoute('album');
     }
 
     public function editAction()
